c1fada935cb371c935955c1310e249eb8c4ae05b
NakagawaKeigo <nakagawa.keigo.21@shizuoka.ac.jp>
2022-06-09 09:08:43 +0900

    headerを追加した

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,41 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <header class="header">
-        <div class="container">
-            <a href="#" class="logo">portfolio</a>
+    <div className={styles.container}>
+      <Head>
+        <title>Myportfolio</title>
+        <meta name='description' content="This is description" />
+        {/*見出し用のフォント*/}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat&display=swap"
+          rel="stylesheet"
+        />
+        {/*本文のフォント*/}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300&display=swap"
+          rel="stylesheet"
+        />
+        {/*小見出し用のフォント*/}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Zen+Maru+Gothic:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <div className={styles.header_container}>
+            <a href="#" className={styles.logo}>portfolio</a>
             <nav>
-                <ul class="nav">
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#">Works</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
+              <ul className={styles.hav}>
+                <li><a href="#about">About</a></li>
+                <li><a href="#">Works</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
             </nav>
-        </div>
-    </header>
+          </div>
+        </header>
+      </main>
+    </div>
   )
 }
